10f50b2416a01ff6c462a1321734892d2c98d350
HarunaYui <ZhyakRHunter@foxmail.com>
2023-10-19 16:57:27 +0800

    UdonLearnFirst

---
+++ Assets/UdonSharp 1/PickUpCube.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PickUpCube : UdonSharpBehaviour
+{
+    public Material targetMaterial;
+    void Start()
+    {
+        targetMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    public override void OnPickupUseDown()
+    {
+        targetMaterial.color = Color.green;
+        Debug.Log("OnPickupUseDown");
+    }
+
+    public override void OnPickupUseUp()
+    {
+        targetMaterial.color = Color.red;
+        Debug.Log("OnPickupUseUp");
+    }
+}
