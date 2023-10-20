a8feac8e889c99c659450f6f62482ff04583a855
HarunaYui <ZhyakRHunter@foxmail.com>
2023-10-20 11:06:01 +0800

    Add ClickCubeChangeMaterials

---
+++ Assets/UdonSharp 1/ClickableCube.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ClickableCube : UdonSharpBehaviour
+{
+    public Material[] materials;
+    private int i = 0;
+    public MeshRenderer meshRenderer;
+    void Start()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        i = (i + 1) % materials.Length;
+        meshRenderer.material = materials[i];
+        Debug.Log($"i = {i}");
+    }
+}
