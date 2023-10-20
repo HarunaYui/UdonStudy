c26ca28dcf0de1ec819895f6673952af8cae6b75
HarunaYui <ZhyakRHunter@foxmail.com>
2023-10-20 14:56:24 +0800

    chairUdon && Interactioncube study

---
+++ Assets/UdonSharp 1/ChairUdon.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ChairUdon : UdonSharpBehaviour
+{
+    private string _playerName;
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        Networking.LocalPlayer.UseAttachedStation();
+        _playerName = Networking.LocalPlayer.displayName;
+    }
+
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        Debug.Log($"Player:{_playerName} has Enter");
+    }
+
+    public override void OnStationExited(VRCPlayerApi player)
+    {
+        Debug.Log($"Player:{_playerName} has Exited");
+    }
+}
---
+++ Assets/UdonSharp 1/InteractCube.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class InteractCube : UdonSharpBehaviour
+{
+    public Material[] materials;
+    private int i = 0;
+    public MeshRenderer meshRenderer;
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        i = (i + 1) % materials.Length;
+        meshRenderer.material = materials[i];
+        Debug.Log($"i = {i}");
+    }
+}
