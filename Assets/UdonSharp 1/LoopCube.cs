019806011cea3704559a1dbd3889d2dc7863d409
HarunaYui <ZhyakRHunter@foxmail.com>
2023-10-20 13:40:24 +0800

    timercube&&LoopCube Study

---
+++ Assets/UdonSharp 1/LoopCube.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LoopCube : UdonSharpBehaviour
+{
+    public Text _text;
+    private string textConcat;
+    void Start()
+    {
+        _text = GetComponentInChildren<Text>();
+    }
+
+    private void OnMouseDown()
+    {
+        textConcat = null;
+        for (int i = 0; i < 9; i++)
+        {
+            textConcat += string.Concat(i);
+        }
+        _text.text = textConcat;
+    }
+}
---
+++ Assets/UdonSharp 1/TimerCube.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TimerCube : UdonSharpBehaviour
+{
+    public Material[] materials;
+    private int i = 0;
+    private float _prevTime;
+    public MeshRenderer meshRenderer;
+    private readonly float _interval = 5f;
+    void Start()
+    {
+        _prevTime = Time.time;
+        Debug.Log($"prevTime = {_prevTime}");
+    }
+
+    private void Update()
+    {
+        if (Time.time - _prevTime >= _interval)
+        {
+            i = (i + 1) % materials.Length;
+            meshRenderer.material = materials[i];
+            _prevTime = Time.time;
+            Debug.Log($"i = {i}");
+        }
+    }
+}
