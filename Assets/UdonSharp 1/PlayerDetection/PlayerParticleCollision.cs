4ed3c6c596a21e0a992dce000831f556ab30b6e7
HarunaYui <ZhyakRHunter@foxmail.com>
2023-10-23 16:57:14 +0800

    Add PlayerParticleCollsion

---
+++ Assets/UdonSharp 1/PlayerDetection/PlayerParticleCollision.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayerParticleCollision : UdonSharpBehaviour
+{
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        target.SetActive(true);
+        Debug.Log($"{player.displayName} entered");
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        target.SetActive(false);
+        Debug.Log($"{player.displayName} exited");
+    }
+}
